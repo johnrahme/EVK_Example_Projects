5c6b0cf26759bcbaf02dfb59c1aa177914a44f47
johnrahme <john.rahme.se@gmail.com>
2016-11-24 16:19:15 +0100

    Android project and matlab db connection added

---
+++ Matlab/db_connect.m
@@ -0,0 +1,16 @@
+clear all;
+javaaddpath('mysql-connector-java-5.1.40-bin.jar');
+conn = database('uwb','root','','Vendor','MySQL','Server','localhost');
+            
+tablename = 'positions';
+colnames = {'x','y'};
+              
+data = {1,3};
+data_table = cell2table(data,'VariableNames',colnames);
+fastinsert(conn,tablename,colnames,data_table);
+
+sqlquery = 'select * from positions';
+
+curs = exec(conn,sqlquery);
+curs = fetch(curs);
+curs.Data
