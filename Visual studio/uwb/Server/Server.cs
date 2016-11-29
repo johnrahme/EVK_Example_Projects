062abfa968109ccbee425e940c99fb26af39cf8e
johnrahme <john.rahme.se@gmail.com>
2016-11-29 01:21:29 +0100

    Visual studio projekt tillagd

---
+++ Visual studio/uwb/Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using uwb;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost sh = new ServiceHost(typeof(Service1));
+            sh.Open();
+            Console.WriteLine("Running");
+            Console.ReadLine();
+            sh.Close();
+        }
+    }
+}
