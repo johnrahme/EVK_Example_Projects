4bf5fea187d263c1d544ae0097b26d5d4cebc3af
johnrahme <john.rahme.se@gmail.com>
2016-12-06 14:08:52 +0100

    Testing db upload speed

--- Matlab/simulateDB.m
+++ Matlab/simulateDB.m
@@ -7,18 +7,21 @@ numberOfUpdates = 300;
 i = 0;
 x = 0;
 y = 0;
-dx = 0.01;
-dy = 0.01;
+dx = 5;
+dy = 5;
+A = [];
 while(i<numberOfUpdates)
     if(i == 150)
-        dx = -0.01;
+        dx = 5;
     end
     x = x+dx;
     y = y+dy;
     
     data = {x,y,1};
     data_table = cell2table(data,'VariableNames',colnames);
+    tic;
     fastinsert(conn,tablename,colnames,data_table);
+    A = [A toc];
     pause(0.1);
     i = i+1;
 end
